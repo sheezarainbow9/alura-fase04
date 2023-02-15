8182e34603147af3ceba24979a36fae4b0fb867b
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-15 11:20:13 -0300

    for

--- javascript/8-loops.js
+++ javascript/8-loops.js
@@ -2,15 +2,27 @@
 
 const listaDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
-const idadeComprador = 18;
-const acompanhado = true;
+const idadeComprador = 17;
+const acompanhado = false;
 let passagemComprada = false;
-const destino = "São Paulo";
+const destino = "Curitiba";
 
 console.log(` Destinos possíveis: \n`);
 console.log(listaDestinos);
 
 const podeComprar = idadeComprador >= 18 || acompanhado == true;
+
+// let contador = 0;
+// let destinoExiste = false;
+// while (contador < 3) {
+//   if (listaDestinos[contador] == destino) {
+//     destinoExiste = true;
+//     break;
+//   }
+//   contador += 1;
+// }
+
+// console.log("Destino existe.");
 
 let contador = 0;
 let destinoExiste = false;
@@ -22,4 +34,17 @@ while (contador < 3) {
   contador += 1;
 }
 
-console.log("Destino existe.");
+console.log("Destino existe:", destinoExiste);
+
+if (podeComprar && destinoExiste) {
+  console.log("Boa viagem!");
+} else {
+  console.log("Desculpe, tivemos um erro.");
+}
+
+// for - (inicialização; condição; incremento)
+for (let i = 0; i < 3; i++) {
+  if (listaDestinos[i] == destino) {
+    destinoExiste = true;
+  }
+}
