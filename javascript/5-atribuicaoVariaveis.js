abbc04df5ec038b7c0ccdd943f9786840ed5e6d1
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-15 09:36:25 -0300

    renamed files

--- javascript/5-atribuicaoVariaveis.js
+++ javascript/5-atribuicaoVariaveis.js
@@ -1,6 +1,6 @@
 // Atribuição de variáveis:
 
-const idade = 29;
+//const idade = 29;
 //let nome = "Ricardo";
 const primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
@@ -12,5 +12,10 @@ console.log(primeiroNome + " " + sobrenome);
 console.log(`Meu nome é ${primeiroNome} ${sobrenome}`); // interpolação.
 
 // só sobrescrever se for 'let' na variável de mesmo nome. 'const' não deixa mudar.
-nome = primeiroNome + sobrenome;
-console.log(nome);
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável.
+idade = 26; //atribuindo valor.
+idade = idade + 1;
+console.log(idade);
