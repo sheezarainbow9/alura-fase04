1d63f4f6047c4ab8fd63939b0c177a40c9926880
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-15 09:49:49 -0300

    listas

---
+++ javascript/6-listas.js
@@ -0,0 +1,17 @@
+// Listas:
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+    
+);
+
+listaDestinos.push(`Curitiba`); // adiciona item na lista.
+
+console.log(`Destinos possíveis:`);
+console.log(listaDestinos);
