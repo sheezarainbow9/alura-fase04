e0d823c611595e12b5468641cb0b6e98bfe9df0e
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-16 12:33:45 -0300

    error message, readme

--- validacao-doguito-main/README.md
+++ validacao-doguito-main/README.md
@@ -15,3 +15,11 @@
 # Utilizar data attributes para trabalhar com JavaScript
 
 # Deixar a função de validação mais genérica para funcionar com qualquer input
+
+# A instalar e usar o plugin Liveserver
+
+# Como baixar e rodar o browser-sync
+
+# Como mostrar a mensagem de erro diretamente no HTML
+
+# Como customizar as mensagens de erro de validação
--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -3,12 +3,58 @@ export function valida(input) {
 
   if (validadores[tipoInput]) {
     validadores[tipoInput](input);
-  };
+  }
 
   const validadores = {
-    dataNascimento: input => validaDataNascimento(input),
+    dataNascimento: (input) => validaDataNascimento(input),
   };
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "";
+  } else {
+    input.parentElement.classList.add("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML =
+      mostraMsgErro(tipo, tipoInput);
+  }
+}
+
+const tiposErro = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError",
+];
+
+const mensagemErro = {
+  nome: {
+    valueMissing: "O campo nome não pode estar vazio.",
+  },
+  email: {
+    valueMissing: "O campo email não pode estar vazio.",
+    typeMismatch: "O email digitado não é válido.",
+  },
+  senha: {
+    valueMissing: "O campo senha não pode estar vazio.",
+    typeMismatch:
+      "A senha deve conter entre 6 e 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.",
+  },
+  dataNascimento: {
+    valueMissing: "O campo data de nascimento não pode estar vazio.",
+    customError: "Você deve ser maior de 18 anos para se cadastrar.",
+  },
 };
+
+function mostraMsgErro(tipoInput, input) {
+  let mensagem = "";
+  tiposErro.forEach(erro => {
+    if (input.validity[erro]) {
+      mensagem = mensagemErro[tipoInput][erro];
+    }
+  });
+  
+  return mensagem;
+}
 
 function validaDataNascimento(input) {
   const dataRecebida = new Date(input.value);
