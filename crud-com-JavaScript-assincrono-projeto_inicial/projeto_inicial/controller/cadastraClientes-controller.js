91d6ef991aaf3498adcd20344052ed990683805f
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-24 15:11:28 -0300

    erro de sintaxe

--- crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/controller/cadastraClientes-controller.js
+++ crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/controller/cadastraClientes-controller.js
@@ -7,7 +7,7 @@ formulario.addEventListener("submit", (evento) => {
   const nome = evento.target.querySelector("[data-nome]").value;
   const email = evento.target.querySelector("[data-email]").value;
 
-  clienteService.criaCliente(nome, email);
+  clienteService.criaCliente(nome, email)
   .then(() => {
     window.location.href = "../telas/cadastro_concluido.html";
   });
